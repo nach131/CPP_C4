3adbeeb04a4bdbdd0f4558846629ffd626575e88
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-12-12 19:00:23 +0100

    upgrade

--- Module 00/ex00/inc/megaphone.hpp
+++ Module 00/ex00/inc/megaphone.hpp
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.hpp                                      :+:      :+:    :+:   */
+/*   Megaphone.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:10:13 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/11/27 17:11:37 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:36:45 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MEGAPHONE_H
-# define MEGAPHONE_H
+#ifndef MEGAPHONE_HPP
+#define MEGAPHONE_HPP
 
-# include <iostream>
-# define CYAN "\033[0;36m"
-# define ERROR "\x1B[41m\x1B[33m"
-# define RESET "\x1B[0m"
+#include <iostream>
+
+#define CYAN "\033[0;36m"
+#define ERROR "\x1B[41m\x1B[33m"
+#define RESET "\x1B[0m"
 
 #endif
--- Module 00/ex00/src/megaphone.cpp
+++ Module 00/ex00/src/megaphone.cpp
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 12:10:16 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/11/27 17:11:41 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:33:18 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/megaphone.hpp"
+#include "Megaphone.hpp"
 
-int	main(int n, char **str)
+int main(int n, char **str)
 {
 	if (n <= 1)
 		std::cout << ERROR << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << RESET;
