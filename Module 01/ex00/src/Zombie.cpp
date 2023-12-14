6417119ca4070137253a59fdf652547281d317b8
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-12-14 19:47:34 +0100

    en sus archivos

--- Module 01/ex00/src/Zombie.cpp
+++ Module 01/ex00/src/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 22:32:38 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/12/14 19:05:55 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:46:20 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,3 @@ Zombie::Zombie(std::string name) { _name = name; }
 Zombie::~Zombie() { std::cout << _name << " destroyed." << std::endl; }
 
 void Zombie::announce() { std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl; }
-
-Zombie *newZombie(std::string name) { return new Zombie(name); }
-
-void randomChump(std::string name)
-{
-	Zombie tmp(name);
-	tmp.announce();
-}
