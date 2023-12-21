a186599e05348fc93ff1c4524b774789d794cfdf
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-12-21 23:10:21 +0100

    cleaning

---
+++ Module 01/ex04/inc/colorsft.hpp
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET "\x1B[0m"
+
+// # Regular Colors
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\x1B[38;2;47;82;191m"
+#define MAGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define ORANGE "\033[1;31m"
+#define WHITE "\033[0;37m"
+#define BLACK "\x1B[30m"
+
+#endif
--- Module 01/ex04/src/main.cpp
+++ Module 01/ex04/src/main.cpp
@@ -6,30 +6,28 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 22:22:20 by nmota-bu          #+#    #+#             */
-/*   Updated: 2023/12/21 19:02:58 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2023/12/21 19:54:46 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-#include "Weapon.hpp"
+#include <iostream>
+#include <colorsft.hpp>
 
-int main()
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define MAGENTA "\033[0;35m"
+#define RESET "\x1B[0m"
+
+int main(int n, char **str)
 {
+	if (n == 4)
 	{
-		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
+		std::string file = str[1];
+		std::string s1 = str[2];
+		std::string s2 = str[3];
+		std::cout << GREEN << file << std::endl;
+		std::cout << YELLOW << s1 << std::endl;
+		std::cout << MAGENTA << s2 << RESET << std::endl;
 	}
 	return 0;
 }
