e0569a7ddf25ffef72d2d151e389b1885c7d2d29
Nacho Mota 🚀 <nacho.mota@gmail.com>
2024-01-07 18:30:03 +0100

    ex00 ok

--- Module_04/ex00/src/main.cpp
+++ Module_04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 22:22:20 by nmota-bu          #+#    #+#             */
-/*   Updated: 2024/01/07 15:08:55 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2024/01/07 18:29:04 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "Colors_ft.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main(void)
 {
@@ -33,10 +35,23 @@ int main(void)
 		delete meta;
 		delete j;
 		delete i;
-		// return 0;
 	}
 	{
 		std::cout << "\t\t" << ERROR << "[ Wrong Class ]" << RESET << std::endl;
+
+		const WrongAnimal *meta = new WrongAnimal();
+		// const Animal *j = new Dog();
+		const WrongAnimal *i = new WrongCat();
+
+		// std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+
+		i->makeSound(); // will output the cat sound! j->makeSound();
+		// j->makeSound();
+		meta->makeSound();
+
+		delete meta;
+		delete i;
 	}
 	return 0;
 }
